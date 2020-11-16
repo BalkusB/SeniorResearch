3008af45c60385fdf72b095017b9fd31cd627579
BalkusB <73603878+BalkusB@users.noreply.github.com>
2020-11-15 20:54:18 -0500

    Add files via upload

--- CAEvac.java
+++ CAEvac.java
@@ -17,7 +17,21 @@ public class CAEvac
 		obj.createClassroom();
 		obj.setDoorDistances(0);
 		obj.visualizeRoom();
-		System.out.println("Evacuation Time: " + obj.simulate());
+		System.out.println("Evacuation Time: " + obj.bulkSim(1));
+	}
+	
+	public double bulkSim(double trials)
+	{
+		double toRet = 0;
+		for(int i = 0; i < trials; i++)
+		{
+			toRet += simulate();
+			currentRoom.clear();
+			createClassroom();
+			setDoorDistances(0);
+			System.out.println(i);
+		}
+		return toRet / trials;
 	}
 	
 	public int simulate()
@@ -51,13 +65,13 @@ public class CAEvac
 			{
 				if(!t.getOccupied() && t.getOccupiable() == true)
 				{
-					//Random r = new Random();
-					//int panic = r.nextInt(20);
+					Random r = new Random();
+					int panic = r.nextInt(20);
 					for(Tile t2 : currentRoom)
 					{
 						int xdis = Math.abs(t.getX() - t2.getX());
 						int ydis = Math.abs(t.getY() - t2.getY());
-						if(t2.getDistanceFromDoor() > t.getDistanceFromDoor() && t2.getOccupied() == true && xdis <= 1 && ydis <= 1)
+						if(t2.getDistanceFromDoor() > t.getDistanceFromDoor() && t2.getOccupied() == true && xdis <= 1 && ydis <= 1 && panic != 0)
 						{
 							t.setOccupied(true);
 							t2.setOccupied(false);
@@ -82,15 +96,15 @@ public class CAEvac
 	
 	public void createClassroom()
 	{
-		roomWidth = 7;
-		roomLength = 7;
-		doorX = 3;
-		doorY = 0;
-		for(int i = 0; i < roomWidth; i++)
+		roomWidth = 20;
+		roomLength = 16;
+		doorX = 0;
+		doorY = 2;
+		for(int i = 0; i < roomLength; i++)
 		{
-			for(int i2 = 0; i2 < roomLength; i2++)
+			for(int i2 = 0; i2 < roomWidth; i2++)
 			{
-				currentRoom.add(new Tile(true, false, i, i2));
+				currentRoom.add(new Tile(true, false, i2, i));
 			}
 		}
 		
@@ -103,16 +117,41 @@ public class CAEvac
 			if(t.getX() == doorX && t.getY() == doorY)
 				t.setOccupiable(true);
 		
-		createObstacle(2, 2);
-		createObstacle(2, 4);
-		createObstacle(4, 2);
-		createObstacle(4, 4);
+		createWall(4, 1, 4, 3);
+		createWall(7, 1, 7, 3);
+		createWall(10, 1, 10, 3);
+		createWall(13, 1, 13, 3);
+		createWall(16, 1, 16, 3);
 		
-		createPerson(1, 5);
-		createPerson(2, 5);
-		createPerson(3, 5);
-		createPerson(4, 5);
-		createPerson(5, 5);
+		createWall(4, 6, 4, 9);
+		createWall(7, 6, 7, 9);
+		createWall(10, 6, 10, 9);
+		createWall(13, 6, 13, 9);
+		createWall(16, 6, 16, 9);
+		
+		createWall(4, 12, 4, 14);
+		createWall(7, 12, 7, 14);
+		createWall(10, 12, 10, 14);
+		createWall(13, 12, 13, 14);
+		createWall(16, 12, 16, 14);
+		
+		createPersonGroup(5, 1, 5, 3);
+		createPersonGroup(8, 1, 8, 3);
+		createPersonGroup(11, 1, 11, 3);
+		createPersonGroup(14, 1, 14, 3);
+		createPersonGroup(17, 1, 17, 3);
+		
+		createPersonGroup(5, 6, 5, 9);
+		createPersonGroup(8, 6, 8, 9);
+		createPersonGroup(11, 6, 11, 9);
+		createPersonGroup(14, 6, 14, 9);
+		createPersonGroup(17, 6, 17, 9);
+		
+		createPersonGroup(5, 12, 5, 14);
+		createPersonGroup(8, 12, 8, 14);
+		createPersonGroup(11, 12, 11, 14);
+		createPersonGroup(14, 12, 14, 14);
+		createPersonGroup(17, 12, 17, 14);
 	}
 	
 	public void createObstacle(int x, int y)
@@ -138,9 +177,43 @@ public class CAEvac
 					if(t.getY() == i && t.getX() == i2)
 					{
 						t.setOccupiable(false);
+						t.setDistanceFromDoor(1000);
 					}
 				}
 			}
+		}
+	}
+	
+	public void createPersonGroup(int x1, int y1, int x2, int y2)
+	{
+		for(int i = y1; i <= y2; i++)
+		{
+			for(int i2 = x1; i2 <= x2; i2++)
+			{
+				for(Tile t : currentRoom)
+				{
+					if(t.getY() == i && t.getX() == i2)
+					{
+						t.setOccupied(true);
+					}
+				}
+			}
+		}
+	}
+	
+	public void ditributeRandom(int num)
+	{
+		Collections.shuffle(currentRoom);
+		int i = 0;
+		int i2 = 0;
+		while(i2 < num)
+		{
+			if(currentRoom.get(i).getOccupiable())
+			{
+				currentRoom.get(i).setOccupied(true);
+				i2++;
+			}
+			i++;
 		}
 	}
 	
@@ -157,9 +230,9 @@ public class CAEvac
 	
 	public void visualizeRoom()
 	{
-		for(int i = 0; i < roomWidth; i++)
+		for(int i = 0; i < roomLength; i++)
 		{
-			for(int i2 = 0; i2 < roomLength; i2++)
+			for(int i2 = 0; i2 < roomWidth; i2++)
 			{
 				for(Tile t : currentRoom)
 				{
@@ -179,9 +252,9 @@ public class CAEvac
 	
 	public void visualizeRoom2()
 	{
-		for(int i = 0; i < roomWidth; i++)
+		for(int i = 0; i < roomLength; i++)
 		{
-			for(int i2 = 0; i2 < roomLength; i2++)
+			for(int i2 = 0; i2 < roomWidth; i2++)
 			{
 				for(Tile t : currentRoom)
 				{
